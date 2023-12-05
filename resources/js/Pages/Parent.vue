cafece9fce6bc64f3ab77e6c622f9c440e4e413f
kozomaruyama <kozomaruyama@gmail.com>
2023-12-06 07:58:20 +0900

    20231206

--- resources/js/Pages/Parent.vue
+++ resources/js/Pages/Parent.vue
@@ -15,7 +15,7 @@ import { Head } from '@inertiajs/vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        <v-btn>parent</v-btn>
+                        <v-btn>parent-git</v-btn>
                     </div>
                 </div>
             </div>
